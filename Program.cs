3457aeb348c25c738c60385c6266a21311c3b9ca
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-11 15:05:52 +0300

    DBContext service olarak eklendi

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using SinemaOtomasyonu.Utilitiy;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
