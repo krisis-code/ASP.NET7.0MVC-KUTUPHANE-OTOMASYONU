67df7f42e710b6f5a5c07bb695f85042b1774f06
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-11 15:45:57 +0300

    BookGenresController eklendi

--- obj/Debug/net7.0/SinemaOtomasyonu.AssemblyInfo.cs
+++ obj/Debug/net7.0/SinemaOtomasyonu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SinemaOtomasyonu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3457aeb348c25c738c60385c6266a21311c3b9ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bc8e18a2323fd89d8dc3ccff386d50506cf0086")]
 [assembly: System.Reflection.AssemblyProductAttribute("SinemaOtomasyonu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SinemaOtomasyonu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
