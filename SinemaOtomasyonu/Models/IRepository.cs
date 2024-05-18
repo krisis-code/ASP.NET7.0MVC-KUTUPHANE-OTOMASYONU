215f67e956fe4b6c1fd16ab0b318fe12a5a142e9
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-18 12:41:15 +0300

    repository metodları yazıldı

--- SinemaOtomasyonu/Models/IRepository.cs
+++ SinemaOtomasyonu/Models/IRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace KitapKiralamaOtomasyonu.Models
+﻿using System.Linq.Expressions;
+
+namespace KitapKiralamaOtomasyonu.Models
 {
-	public interface IRepository
+	public interface IRepository<T> where T : class
 	{
+		IEnumerable<T> GetAll();
+		T Get(Expression<Func<T, bool>> filter);
+		void add(T entity);
+		void remove(T entity);
+		void removeBetweem(IEnumerable<T> entities);
 	}
 }
