ef48e2efd11cfa8214ff41cc8603831b3b121b94
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-21 22:11:10 +0300

    Book clasına proplar eklendi

--- Kütüphane Otomasyonu/Models/Book.cs
+++ Kütüphane Otomasyonu/Models/Book.cs
@@ -1,6 +1,18 @@
-﻿namespace KitapKiralamaOtomasyonu.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KitapKiralamaOtomasyonu.Models
 {
     public class Book
     {
+        [Key]
+        public Guid BookId { get; set; }
+        [Required]
+        public string BookName { get; set; }
+        
+        public string Description { get; set; }
+        [Required]
+        public string Writer { get; set; }
+        [Required]
+        public double Price { get; set; }
     }
 }
