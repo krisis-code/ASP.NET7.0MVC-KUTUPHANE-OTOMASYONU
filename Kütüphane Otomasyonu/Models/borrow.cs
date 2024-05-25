e9b67f2418ba590723c039c163f7ffd88a25c275
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-25 13:02:07 +0300

    borrow propları eklendi

--- Kütüphane Otomasyonu/Models/borrow.cs
+++ Kütüphane Otomasyonu/Models/borrow.cs
@@ -1,6 +1,22 @@
-﻿namespace KitapKiralamaOtomasyonu.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KitapKiralamaOtomasyonu.Models
 {
 	public class borrow
 	{
+		[Key]
+		public Guid BorrowId { get; set; }
+
+		[Required]
+		public Guid UserId { get; set; }
+
+		[ValidateNever]
+		public Guid BookId { get; set; }
+		[ForeignKey("BookId")]
+
+		[ValidateNever]
+		public Book book { get; set; }
 	}
 }
