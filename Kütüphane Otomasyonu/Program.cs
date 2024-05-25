53614971f57df789052bef6bf0d2c6ed7e2da1c3
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-25 13:48:00 +0300

    İndex sayfası eklendi

--- Kütüphane Otomasyonu/Program.cs
+++ Kütüphane Otomasyonu/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IBookGenreRepository, BookGenreRepository>();
 builder.Services.AddScoped<IBookRepository, BookRepository>();
+builder.Services.AddScoped<IBorrowRepository, BorrowRepository>();
 
 var app = builder.Build();
 
